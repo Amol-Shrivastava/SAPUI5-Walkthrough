9f12b0dfde1a1692cea5e4a34f60d879a8f47ac0
Amol <amolshrivastava448@gmail.com>
2021-06-12 13:33:56 +0530

    Code to use JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,28 @@
-//Controller Assests
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
 
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onShowHello: function () {
-      // show a native JavaScript alert
-      /* eslint-disable no-alert */
-      alert("Hello World I'm here to rock");
-      /* eslint-enable no-alert */
-    },
-  });
-});
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit: function () {
+        // set data model on view
+        //Data stored in JSON format
+        var oData = {
+          recipient: {
+            name: "Hey World how you doin'?",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
+
+      onShowHello: function () {
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
