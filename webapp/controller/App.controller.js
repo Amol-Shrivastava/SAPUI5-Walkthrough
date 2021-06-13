5162fde3293e4ef50fe8e450882a56a4a4a12941
Amol <amolshrivastava448@gmail.com>
2021-06-13 19:00:28 +0530

    Code for Descriptor for applications

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,27 +1,20 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-  ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      
-      onShowHello: function () {
-        // MessageToast.show("Hello World");
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
-        //Read Message from i18n Model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			// show message
+			MessageToast.show(sMsg);
+		}
+	});
 
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+});
